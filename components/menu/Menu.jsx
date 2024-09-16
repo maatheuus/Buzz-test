07f5685cac053c21f70450fed38b7b921172ec13
MATHEUS <matts14smkd@gmail.com>
2024-09-16 19:35:06 -0300

    add data menu

---
+++ components/menu/Menu.jsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useState } from "react";
+import Button from "./Button";
+import { AnimatePresence, motion } from "framer-motion";
+import LinksMenu from "./LinksMenu";
+import { menu } from "./anim";
+
+export default function Menu() {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <div className="fixed z-50 sm:hidden right-12 top-12">
+      <motion.menu
+        className="w-[348px] h-[650px] bg-black rounded-3xl relative overflow-hidden"
+        variants={menu}
+        animate={isActive ? "open" : "closed"}
+        initial="closed"
+        role="menu"
+        aria-hidden={!isActive}
+      >
+        <AnimatePresence>
+          {isActive && <LinksMenu setIsActive={setIsActive} />}
+        </AnimatePresence>
+      </motion.menu>
+
+      <Button
+        isActive={isActive}
+        toggleMenu={() => {
+          setIsActive(!isActive);
+        }}
+      />
+    </div>
+  );
+}
