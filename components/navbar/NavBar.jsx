fdcc5f4e1fd16afc25cae804104679ca83024181
MATHEUS <matts14smkd@gmail.com>
2024-09-16 19:37:44 -0300

    add links for the navbar

--- components/navbar/NavBar.jsx
+++ components/navbar/NavBar.jsx
@@ -1,3 +1,5 @@
+import { links } from "../menu/links";
+import ScrollSection from "../ui/ScrollSection";
 import Logo from "./Logo";
 
 function NavBar() {
@@ -5,6 +7,19 @@ function NavBar() {
     <nav className="text-white hidden sm:block">
       <div className="flex justify-between gap-4 px-8 py-5">
         <Logo />
+
+        <ul className="flex w-full justify-center list-none gap-x-4">
+          {links.map((item) => (
+            <li key={item.label}>
+              <ScrollSection
+                key={item.to}
+                to={item.to}
+                label={item.label}
+                className="cursor-pointer"
+              />
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   );
