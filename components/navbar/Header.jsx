58ea5fa270c14bc748baf343bb6fdb5f3fa7cb9c
MATHEUS <matts14smkd@gmail.com>
2024-09-16 19:36:39 -0300

    add menu

--- components/navbar/Header.jsx
+++ components/navbar/Header.jsx
@@ -1,9 +1,11 @@
+import Menu from "../menu/Menu";
 import Navbar from "./Navbar";
 
 function Header() {
   return (
     <header className="w-full fixed top-0 z-50 bg-black/30 backdrop-blur-sm">
       <Navbar />
+      <Menu />
     </header>
   );
 }
