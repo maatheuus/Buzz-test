c39f4a4e4c9328a00e41344401f90a7f9ab4e764
MATHEUS <matts14smkd@gmail.com>
2024-09-16 19:47:11 -0300

    add hook for handle with animated text

---
+++ components/hero/useAnimateText.js
@@ -0,0 +1,34 @@
+import { useGSAP } from "@gsap/react";
+import ScrollTrigger from "gsap-trial/dist/ScrollTrigger";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
+
+function useAnimateText(firstText, secondText, slider) {
+  useGSAP(() => {
+    gsap.to(slider.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        scrub: 0.25,
+        start: 0,
+        end: window.innerHeight,
+        onUpdate: (e) => (direction = e.direction * -1),
+      },
+      x: "-500px",
+    });
+    requestAnimationFrame(animate);
+  }, []);
+
+  const animate = () => {
+    if (xPercent < -100) {
+      xPercent = 0;
+    } else if (xPercent > 0) {
+      xPercent = -100;
+    }
+    gsap.set(firstText.current, { xPercent: xPercent });
+    gsap.set(secondText.current, { xPercent: xPercent });
+    requestAnimationFrame(animate);
+    xPercent += 0.1 * direction;
+  };
+}
+
+export default useAnimateText;
