e48bb5fc15167c7038b7fb380163fec57748589a
MATHEUS <matts14smkd@gmail.com>
2024-09-16 19:35:42 -0300

    implementing a scroll section when user click the link

---
+++ components/ui/ScrollSection.jsx
@@ -0,0 +1,48 @@
+import Lenis from "@studio-freight/lenis";
+import { useEffect, useRef } from "react";
+
+function ScrollSection({ to, label, className }) {
+  const lenisRef = useRef(null);
+  useEffect(() => {
+    lenisRef.current = new Lenis({
+      duration: 1,
+      immediate: true,
+      easing: (t) => {
+        return t;
+      },
+    });
+
+    function raf(time) {
+      lenisRef.current.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenisRef.current.destroy();
+    };
+  }, [lenisRef]);
+
+  function handleScroll(to) {
+    if (lenisRef.current) {
+      lenisRef.current.scrollTo(to);
+    }
+  }
+
+  return (
+    <a
+      className={className}
+      href={to}
+      aria-label={`Scroll to ${label}`}
+      onClick={(e) => {
+        e.preventDefault();
+        handleScroll(to);
+      }}
+    >
+      {label}
+    </a>
+  );
+}
+
+export default ScrollSection;
