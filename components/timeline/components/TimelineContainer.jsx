9de514e65dcabc6bd7be42ff5235def7bc149858
MATHEUS <matts14smkd@gmail.com>
2024-09-17 19:13:02 -0300

    add CardTimeline component for display the cards

--- components/timeline/components/TimelineContainer.jsx
+++ components/timeline/components/TimelineContainer.jsx
@@ -1,5 +1,20 @@
-function TimelineContainer() {
-  return <div></div>;
+import { timeLineData } from "../utils";
+import CardTimeline from "./CardTimeline";
+
+function TimelineContainer({ children }) {
+  return (
+    <section
+      id="timeline"
+      aria-labelledby="timeline-heading"
+      className="bg-black relative"
+    >
+      {timeLineData.map((items) => (
+        <CardTimeline key={items.id} data={items} />
+      ))}
+
+      {children}
+    </section>
+  );
 }
 
 export default TimelineContainer;
