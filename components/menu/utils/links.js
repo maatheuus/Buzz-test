539269c118018370860af9e7a83aeb1016210645
MATHEUS <matts14smkd@gmail.com>
2024-09-17 16:29:10 -0300

    change component for a new path

---
+++ components/menu/utils/links.js
@@ -0,0 +1,7 @@
+export const links = [
+  { label: "Home", to: "#home" },
+  { label: "Experience", to: "#experience" },
+  { label: "Buzzvel", to: "#buzzvel" },
+  { label: "Projects", to: "#projects" },
+  { label: "Timeline", to: "#timeline" },
+];
