16dbf09be57f25635ffb7ac0722f8bd50bb3f1c7
MATHEUS <matts14smkd@gmail.com>
2024-09-17 16:53:24 -0300

    import Experience component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import Experience from "@/components/experience/components/Experience";
 import HeroSection from "@/components/hero/HeroSection";
 
 export default function Home() {
   return (
     <div className="relative">
       <HeroSection />
+      <Experience />
     </div>
   );
 }
