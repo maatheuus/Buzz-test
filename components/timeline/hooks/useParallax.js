4a72f122c0d1cc9f37c8292a517639900309bc39
MATHEUS <matts14smkd@gmail.com>
2024-09-17 19:12:32 -0300

    create hook for handle with the progress of the page and animate the cards

---
+++ components/timeline/hooks/useParallax.js
@@ -0,0 +1,6 @@
+import { useTransform } from "framer-motion";
+
+function useParallax(value, distance) {
+  return useTransform(value, [0, 1], [-distance, distance]);
+}
+export default useParallax;
