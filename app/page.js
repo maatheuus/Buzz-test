de4733b41eaa1862791eb85847e00ac60a9afcf7
MATHEUS <matts14smkd@gmail.com>
2024-09-17 18:41:16 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,17 @@
+import AnimatedContentSection from "@/components/buzzvel/components/AnimatedContentSection";
 import Experience from "@/components/experience/components/Experience";
 import HeroSection from "@/components/hero/HeroSection";
 
 export default function Home() {
   return (
-    <div className="relative">
-      <HeroSection />
-      <Experience />
-    </div>
+    <>
+      <div className="relative">
+        <HeroSection />
+        <Experience />
+      </div>
+      <div>
+        <AnimatedContentSection />
+      </div>
+    </>
   );
 }
