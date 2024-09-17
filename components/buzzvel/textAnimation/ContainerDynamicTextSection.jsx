c0e1f1663f5d45e66ccc57ef0bdaa140aad85d5f
MATHEUS <matts14smkd@gmail.com>
2024-09-17 18:36:36 -0300

    add content to the page

--- components/buzzvel/textAnimation/ContainerDynamicTextSection.jsx
+++ components/buzzvel/textAnimation/ContainerDynamicTextSection.jsx
@@ -1,5 +1,24 @@
+"use client";
+
+import {
+  paragraph,
+  paragraphTwo,
+  paragraphThree,
+  paragraphFour,
+} from "../utils/data";
+import Paragraph from "./Paragraph";
+
 function ContainerDynamicTextSection() {
-  return <div></div>;
+  return (
+    <div className="bg-black">
+      <div className="h-[50vh]">
+        <Paragraph paragraph={paragraph} />
+        <Paragraph paragraph={paragraphTwo} />
+        <Paragraph paragraph={paragraphThree} />
+        <Paragraph paragraph={paragraphFour} />
+      </div>
+    </div>
+  );
 }
 
 export default ContainerDynamicTextSection;
