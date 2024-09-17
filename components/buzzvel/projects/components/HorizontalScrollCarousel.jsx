521a9179714ace18ee164e44eae3daa4b4d87998
MATHEUS <matts14smkd@gmail.com>
2024-09-17 18:54:03 -0300

    add data for animation

--- components/buzzvel/projects/components/HorizontalScrollCarousel.jsx
+++ components/buzzvel/projects/components/HorizontalScrollCarousel.jsx
@@ -1,4 +1,13 @@
+import { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+
+import Card from "./Card";
 function HorizontalScrollCarousel() {
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+  });
+
   return <div></div>;
 }
 
