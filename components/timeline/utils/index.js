e3b7ea12355b5c2e2a2c57d8fe737b55b59aa08a
MATHEUS <matts14smkd@gmail.com>
2024-09-17 19:05:38 -0300

    update with data
    update with data for cards

---
+++ components/timeline/utils/index.js
@@ -0,0 +1,68 @@
+export const timeLineData = [
+  {
+    id: 1,
+    year: "Year 1: Getting Started",
+    title: "Fundamentals and Integration",
+    topics: [
+      {
+        title: "Onboarding",
+        description:
+          "During the first year, my focus will be on familiarizing myself with Buzzvel's processes, tools, and teams. Understanding the company culture and adapting to established practices will be essential for an efficient integration. I will also be learning about the company's history, mission, and values.",
+      },
+      {
+        title: "Mentoring",
+        description:
+          "I will work directly with a senior developer, receiving constant guidance and feedback, which will facilitate learning Buzzvel's coding standards and best practices.",
+      },
+      {
+        title: "First Contributions",
+        description:
+          "I will start by taking on small tasks and projects, applying what I learned during onboarding and adjusting to the team's workflow, with the goal of mastering the company's development cycle.",
+      },
+    ],
+  },
+  {
+    id: 2,
+    year: "Year 2-3: Leveling Up",
+    title: "Growth and Increased Responsibility",
+    topics: [
+      {
+        title: "Project Leadership",
+        description:
+          "In the second and third year, I intend to lead small to medium-sized projects, applying the knowledge acquired in the first months and starting to develop autonomy within the development teams.",
+      },
+      {
+        title: "Skills Development",
+        description:
+          "This period will be focused on deepening advanced front-end concepts, as well as learning new technologies that are relevant to Buzzvel's stack, ensuring that I am prepared to take on bigger challenges.",
+      },
+      {
+        title: "Mentorship",
+        description:
+          "As I gain experience, I will be ready to share knowledge, start mentoring junior developers and contribute to the growth of the team.",
+      },
+    ],
+  },
+  {
+    id: 3,
+    year: "Year 4-5: Leading the Way",
+    title: "Leadership and Innovation",
+    topics: [
+      {
+        title: "Leadership Role",
+        description:
+          "By year four, my goal is to be in a position to take on a leadership role, such as a team lead or senior developer, helping to guide the technical and strategic direction of important projects at Buzzvel.",
+      },
+      {
+        title: "Innovation",
+        description:
+          "I will contribute to continuous front-end and full-stack innovation by proposing new ideas and technologies that can improve process efficiency and user experience.",
+      },
+      {
+        title: "Continuous Learning",
+        description:
+          "I will continue to stay up to date with the latest industry trends and innovations, applying these new practices at Buzzvel to ensure the company is at the forefront of web development.",
+      },
+    ],
+  },
+];
