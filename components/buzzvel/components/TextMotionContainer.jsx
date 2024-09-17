c0b8ef47a8755d4632f991b2693baa9d211e7c96
MATHEUS <matts14smkd@gmail.com>
2024-09-17 18:39:02 -0300

    add param to the scroll progress

--- components/buzzvel/components/TextMotionContainer.jsx
+++ components/buzzvel/components/TextMotionContainer.jsx
@@ -1,8 +1,8 @@
 import { motion, useTransform } from "framer-motion";
 
-import ContainerDynamicSection from "../textAnimation/ContainerDynamicSection";
+import ContainerDynamicSection from "../textAnimation/ContainerDynamicTextSection";
 
-function TextMotionContainer() {
+function TextMotionContainer({ scrollYProgress }) {
   const scale = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
   const rotate = useTransform(scrollYProgress, [0, 1], [5, 0]);
 
