306e5d594e07b84082dd8813f45abe818ec3eb10
MATHEUS <matts14smkd@gmail.com>
2024-09-17 19:31:53 -0300

    update page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import { motion } from "framer-motion";
 import AnimatedContentSection from "@/components/buzzvel/animationSection/components/AnimatedContentSection";
 import FeaturedProjectsSection from "@/components/buzzvel/projects/components/FeaturedProjectsSection";
 import Experience from "@/components/experience/components/Experience";
 import HeroSection from "@/components/hero/HeroSection";
+import TimelineContainer from "@/components/timeline/components/TimelineContainer";
 
 export default function Home() {
   return (
@@ -16,6 +18,17 @@ export default function Home() {
 
       <div className="bg-white relative h-[200dvh]">
         <FeaturedProjectsSection />
+      </div>
+
+      <div className="relative">
+        <TimelineContainer>
+          <motion.div
+            className="sticky left-0 right-0 h-1 rounded-full overflow-hidden bottom-24 bg-red-700 z-50"
+            style={{
+              scaleX,
+            }}
+          />
+        </TimelineContainer>
       </div>
     </>
   );
