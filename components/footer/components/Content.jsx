4a1413c0af15ed07c9cc8651e24b6fd77ef172f8
MATHEUS <matts14smkd@gmail.com>
2024-09-17 19:33:57 -0300

    add components for container

---
+++ components/footer/components/Content.jsx
@@ -0,0 +1,13 @@
+import Links from "./Links";
+import TextSection from "./TextSection";
+
+function Content() {
+  return (
+    <div className="legal-layout bg-black py-8 px-12 h-full w-full flex flex-col justify-between">
+      <Links />
+      <TextSection />
+    </div>
+  );
+}
+
+export default Content;
