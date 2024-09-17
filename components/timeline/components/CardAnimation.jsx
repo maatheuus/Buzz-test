b4dc73bd56b79ba662448f9cefd08b9a7ef44401
MATHEUS <matts14smkd@gmail.com>
2024-09-17 19:06:23 -0300

    add animations variables

--- components/timeline/components/CardAnimation.jsx
+++ components/timeline/components/CardAnimation.jsx
@@ -1,4 +1,22 @@
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
+
 function CardAnimation() {
+  const ref = useRef();
+
+  const inView = useInView(ref, {
+    threshold: 0.5,
+    triggerOnce: false,
+  });
+
+  const variants = {
+    visible: { opacity: 1, scale: 1, y: 0 },
+    hidden: {
+      opacity: 0,
+      scale: 0.65,
+      y: 50,
+    },
+  };
   return <div></div>;
 }
 
