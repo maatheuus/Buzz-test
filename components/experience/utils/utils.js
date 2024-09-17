294087dde65531ad8e8b373a06c30cdad025a664
MATHEUS <matts14smkd@gmail.com>
2024-09-17 16:52:35 -0300

    export constants data

---
+++ components/experience/utils/utils.js
@@ -0,0 +1,10 @@
+export const colors = ["#23319fdf", "#f28538d7", "#d62856d7", "#000000"];
+
+export const DRAG_BUFFER = 50;
+
+export const SPRING_OPTIONS = {
+  type: "spring",
+  mass: 3,
+  stiffness: 400,
+  damping: 50,
+};
