f5c8763ba341629c0484eb8f7ce757adca153671
MATHEUS <matts14smkd@gmail.com>
2024-09-17 16:29:58 -0300

    change the path

--- components/menu/links.js
+++
@@ -1,7 +0,0 @@
-export const links = [
-  { label: "Home", to: "#home" },
-  { label: "Experience", to: "#experience" },
-  { label: "Buzzvel", to: "#buzzvel" },
-  { label: "Projects", to: "#projects" },
-  { label: "Timeline", to: "#timeline" },
-];
