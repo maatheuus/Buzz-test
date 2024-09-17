c39d1470f291a1d7ea52711ed823aec8e92038be
MATHEUS <matts14smkd@gmail.com>
2024-09-17 19:08:59 -0300

    add animations variables

--- components/timeline/components/CardTimeline.jsx
+++ components/timeline/components/CardTimeline.jsx
@@ -1,4 +1,12 @@
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
+import CardAnimation from "./CardAnimation";
+
 function CardTimeline() {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({ target: ref });
+  const y = useParallax(scrollYProgress, 300);
+
   return <div></div>;
 }
 
