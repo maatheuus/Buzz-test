56a9fc52a87b926384af9e20a114ed5958700a42
MATHEUS <matts14smkd@gmail.com>
2024-09-17 21:13:35 -0300

    add content

--- components/loader/components/Preloader.jsx
+++ components/loader/components/Preloader.jsx
@@ -26,7 +26,36 @@ function Preloader() {
 
   const curve = usePreloader(dimension);
 
-  return <div></div>;
+  return (
+    <motion.div
+      variants={slideUp}
+      initial="initial"
+      exit="exit"
+      className="h-screen w-screen flex items-center justify-center fixed z-50 bg-[#141516]"
+    >
+      {dimension.width > 0 && (
+        <>
+          <motion.p
+            className="flex text-white text-4xl absolute z-10 items-center"
+            variants={opacity}
+            initial="initial"
+            animate="enter"
+          >
+            <span className="block w-2 h-2 bg-white rounded-full mr-2"></span>
+            {words[index]}
+          </motion.p>
+          <svg className="absolute top-0 w-full h-[calc(100%_+_300px)]">
+            <motion.path
+              className="fill-[#141516]"
+              variants={curve}
+              initial="initial"
+              exit="exit"
+            ></motion.path>
+          </svg>
+        </>
+      )}
+    </motion.div>
+  );
 }
 
 export default Preloader;
