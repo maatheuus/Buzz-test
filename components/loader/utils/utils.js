d71ca6d3bab2ac38963ef800891eabcc5c1ad898
MATHEUS <matts14smkd@gmail.com>
2024-09-17 21:07:40 -0300

    add words data for preloader

---
+++ components/loader/utils/utils.js
@@ -0,0 +1,12 @@
+export const words = [
+  "Hello",
+  "Developer",
+  "Full-Stack",
+  "Front End",
+  "Framer Motion",
+  "GSAP",
+  "React.js",
+  "Next.js",
+  "JavaScript",
+  "Tailwind CSS",
+];
